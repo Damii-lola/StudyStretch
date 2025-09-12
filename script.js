e49956a807a199e87c2a7365be7c1ca55921bc65
Damiii <78701527+Damii-lola@users.noreply.github.com>
2025-09-12 11:49:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.getElementById("testBtn").addEventListener("click", async () => {
-  const res = await fetch("/.netlify/functions/hello");
-  const data = await res.json();
-  document.getElementById("response").innerText = data.message;
-});
+
